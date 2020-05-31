04f24f81b15c91d3b9015f7f71cda36508b0b655
errdaychill <errdaychill@snu.ac.kr>
2020-05-31 22:26:11 +0900

    organized resnet34

--- TLforSR.py
+++ TLforSR.py
@@ -9,16 +9,16 @@ import os
 
 class BasicBlock(nn.Module):
     # initailize layers
-    def __init__(self,in_channel,channel_1,):
+    def __init__(self,in_channel,channel_1,stride=1):
         # get mother class 생성자
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channel,channel_1,3,padding=1)
+        self.conv1 = nn.Conv2d(in_channel, channel_1, 3, stride=stride, padding=1)
         self.bn1 = nn.BatchNorm2d(channel_1)        
-        self.conv2 = nn.Conv2d(channel_1,channel_1,3,padding=1)
+        self.conv2 = nn.Conv2d(channel_1, channel_1, 3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(channel_1)
-        self.identity = nn.Sequential()
-        #if in_channel != channel_1:
-        #   self.identity = nn.Sequential(nn.Conv2d(in_channel, channel_1, 1), nn.BatchNorm2d(channe_1))
+        self.shortcut = nn.Sequential()
+        # if in_channel != channel_1:
+        #    self.shortcut = nn.Sequential(nn.Conv2d(in_channel, channel_1, 1), nn.BatchNorm2d(channe_1))
 
     #link all layers through activate f 
     def forward(self, x):
@@ -26,32 +26,34 @@ class BasicBlock(nn.Module):
         h2_relu = self.conv2(h1_relu)
         # 왜 배치놂한 거 위에 더하지 === 논문에 나와있다
         return F.relu(self.bn2(h2_relu) + self.identity)
-
+    
 class ResNet34(nn.Module):
     def __init__(self,x):
         super().__init__()
-        self.conv0 = F.Conv2d()
-        self.res1 = BasicBlock(64,64)
-        self.res2 = BasicBlock(64,64)
-        self.res3 = BasicBlock(64,64)
-        self.res4 = BasicBlock(64,128)
-        self.res5 = BasicBlock(128,128)
-        self.res6 = BasicBlock(128,128)
-        self.res7 = BasicBlock(128,128)
-        self.res8 = BasicBlock(128,256)
-        self.res9 = BasicBlock(256,256)
-        self.res10 = BasicBlock(256,256)
-        self.res10 = BasicBlock(256,256)
-        self.res10 = BasicBlock(256,256)
-        self.res10 = BasicBlock(256,256)
-        self.res14 = BasicBlock(256,512)
-        self.res15 = BasicBlock(512,512)
-        self.res16 = BasicBlock(512,512)
+        # layer  = Values
+        self.conv1 = nn.Conv2d(in_channel, 64, 7, stride=2, padding=3)
+        # self.maxpool = nn.MaxPool2d(self.conv0
+        self.seq_64 = nn.Sequential(BasicBlock(64,64), BasicBlock(64,64), BasicBlock(64,64)
+        self.seq_128 = nn.Sequential(BasicBlock(64,128,2),BasicBlock(128,128),BasicBlock(128,128),BasicBlock(128,128))
+        self.seq_256 = nn.Sequential(BasicBlock(128,256,2),BasicBlock(256,256),BasicBlock(256,256),
+        BasicBlock(256,256),BasicBlock(256,256),BasicBlock(256,256))
+        self.seq_512 = nn.Sequential(BasicBlock(256,512,2), BasicBlock(512,512), BasicBlock(512,512)) 
         self.avgpool = nn.AvgPool2d(512,512)
+        self.fc_softmax = nn.Sequential(nn.Liear(self.avgpool.shape[0]))
+            
 
     def forward(self ,x):
+        conv1 = self.conv1(x)
+        conv2_input =F.MaxPool2d(conv1)
+        conv2_x = self.seq_64(conv2_input)
+        conv3_x = self.seq_128(conv2_x)
+        conv4_x = self.seq_256(conv3_x)
+        conv5_x = self.seq_512(conv4_x)
 
-
+        # global averagepool
+        avgpool = F.AvgPool2d(conv5_x)
+        #FC dimension depends on CIFAR - X
+        
 
 if __name__ == "__name__":
     ResNet34()
