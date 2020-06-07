87ed28a1c213f79c3dd5362ba5c9b3d63eab8924
errdaychill <errdaychill@snu.ac.kr>
2020-06-07 23:27:36 +0900

    revised

--- TLforSR.py
+++ TLforSR.py
@@ -5,6 +5,17 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+# We
+# use SGD with a mini-batch size of 256. The learning rate
+# starts from 0.1 and is divided by 10 when the error plateaus,
+# and the models are trained for up to 60 × 104
+# iterations. We
+# use a weight decay of 0.0001 and a momentum of 0.9
+
+# In testing, for comparison studies we adopt the standard
+# 10-crop testing [21]. For best results, we adopt the fullyconvolutional form as in [40, 12], and average the scores
+# at multiple scales (images are resized such that the shorter
+# side is in {224, 256, 384, 480, 640})
 
 
 class BasicBlock(nn.Module):
@@ -28,7 +39,7 @@ class BasicBlock(nn.Module):
         return F.relu(self.bn2(h2_relu) + self.identity)
     
 class ResNet34(nn.Module):
-    def __init__(self,x):
+    def __init__(self,x,classes=10):
         super().__init__()
         # layer  = Values
         self.conv1 = nn.Conv2d(in_channel, 64, 7, stride=2, padding=3)
@@ -39,9 +50,9 @@ class ResNet34(nn.Module):
         BasicBlock(256,256),BasicBlock(256,256),BasicBlock(256,256))
         self.seq_512 = nn.Sequential(BasicBlock(256,512,2), BasicBlock(512,512), BasicBlock(512,512)) 
         self.avgpool = nn.AvgPool2d(512,512)
-        self.fc_softmax = nn.Sequential(nn.Liear(self.avgpool.shape[0]))
+        self.fc_layer = nn.Linear(self.avgpool.shape[0], classes)
             
-
+        dddd
     def forward(self ,x):
         conv1 = self.conv1(x)
         conv2_input =F.MaxPool2d(conv1)
@@ -51,9 +62,12 @@ class ResNet34(nn.Module):
         conv5_x = self.seq_512(conv4_x)
 
         # global averagepool
-        avgpool = F.AvgPool2d(conv5_x)
         #FC dimension depends on CIFAR - X
+        avgpool = F.AvgPool2d(conv5_x)
+        scores = self.fc_layer(avgpool)
         
+        loss = nn.CrossEntropyLoss
+    
 
 if __name__ == "__name__":
     ResNet34()
